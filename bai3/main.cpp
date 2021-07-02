d91b56a61a4a522a06bdfe454230c7a21ec56c63
huong <ndh1499@gmail.com>
2021-07-02 15:35:51 +0700

    Initial commit

---
+++ bai3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bai3
+//
+//  Created by Nguyen Dinh Huong on 27/06/2021.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main(){
+    int a = 5;
+    int b = 6;
+    int sum;
+    sum = a+b;
+    printf ("Sum = %d + %d = %d",a,b,sum);
+    
+    getchar();
+}
